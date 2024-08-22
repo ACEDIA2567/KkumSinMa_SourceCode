7cf56bea98c1ca0ec6d1a3f1e2bac24ea4d48e27
ACEDIA2567 <101154683+ACEDIA2567@users.noreply.github.com>
2024-08-22 09:59:24 +0900

    Add files via upload

---
+++ Script/3_Tutorial/TutorialItem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialItem : TutorialBase
+{
+    [SerializeField] private GameObject stick;
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Execute(TutorialController controller)
+    {
+        if (GiveItem())
+        {
+            controller.SetNextTutorial();
+        }
+    }
+
+    private bool GiveItem()
+    {
+        Item item = Managers.Resource.Load<Item>("ItemData/defaultWeapon");
+        Managers.Game.player.Inventory.Equip(item);
+
+        return true;
+    }
+
+    public override void Exit()
+    {
+        stick.SetActive(false);
+        Managers.UI.CloseAllPopupUI();
+    }
+}
