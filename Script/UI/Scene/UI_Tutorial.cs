3cf7a18b8da01cfe191cf808114843dd74a3fb4b
ACEDIA2567 <101154683+ACEDIA2567@users.noreply.github.com>
2024-08-22 10:06:34 +0900

    Add files via upload

---
+++ Script/UI/Scene/UI_Tutorial.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_Tutorial : UI_Scene
+{
+    enum Buttons
+    {
+        SkipBtn,
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        Bind<Button>(typeof(Buttons));
+        
+        GetButton((int)Buttons.SkipBtn).gameObject.BindEvent(Skip);
+        canvas.sortingOrder = 25;
+    }
+
+    void Skip(PointerEventData obj)
+    {
+        Managers.Direction.DestroyTutorial();
+    }
+}
